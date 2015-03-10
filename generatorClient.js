616fda4baf8a4f35abe60a644d9380b11aad527f
eniallator <eniallatormc@gmail.com>
2015-03-10 19:36:44 +0000

    refactoring

--- generatorClient.js
+++ generatorClient.js
@@ -1,44 +1,36 @@
-function parseCommands(inputCommandsEl, outputCommandEl){
-
-	var prefix = "summon FallingSand ~ ~1 ~ {Time:1";
+$(document).ready(function () {
 
 	function getRidingBlock(userCommand){
 		return ",Riding:{id:FallingSand,Time:1,Block:command_block,TileEntityData:{Command:" + userCommand + "}";
-	};
-
-	var inputCommandsValue = inputCommandsEl.value;
-
-	var arrayCommands = inputCommandsValue.split("\n").reverse();
-
-	var numberFillBlocks = arrayCommands.length + 1;
-
-	var outputCommand = "";
-
-	// var outputCommandChars = "";
-
-	var curlyBrackets = "";
-
-	arrayCommands.unshift("fill ~ ~1 ~ ~ ~-" + numberFillBlocks + " ~1 air");
-
-	arrayCommands.push("fill ~ ~1 ~1 ~ ~" + numberFillBlocks + " ~1 redstone_block");
-
-	outputCommand += prefix;
-
-	arrayCommands.forEach(function (val){
-		outputCommand += getRidingBlock(val);
-	});
-	
-	for (var j = 0 ; j < arrayCommands.length + 1;j++){
-		curlyBrackets += "}";
 	}
 
-	$('.outputContainer').removeClass("hidden");
+	function parseCommands(inputCommandsEl, outputCommandEl){
+		var prefix = "summon FallingSand ~ ~1 ~ {Time:1";
+		var inputCommandsValue = inputCommandsEl.value;
+		var arrayCommands = inputCommandsValue.split("\n").reverse();
+		var numberFillBlocks = arrayCommands.length + 1;
+		var outputCommand = "";
 
-	outputCommand += curlyBrackets;
+		arrayCommands.unshift("fill ~ ~1 ~ ~ ~-" + numberFillBlocks + " ~1 air");
+		arrayCommands.push("fill ~ ~1 ~1 ~ ~" + numberFillBlocks + " ~1 redstone_block");
 
-	// outputCommandChars += "There are " + outputCommand.length + " characters in your following command:";
+		outputCommand += prefix;
 
-	outputCommandEl.value = outputCommand;
+		arrayCommands.forEach(function (val){
+			outputCommand += getRidingBlock(val);
+		});
+		
+		for (var j = 0 ; j < arrayCommands.length + 1;j++){
+			outputCommand += "}";
+		}
 
-	// outputCommandCharsEl.value = outputCommandChars
-}
+		$('.outputContainer').removeClass("hidden");
+
+		outputCommandEl.value = outputCommand;
+	}
+
+	$(".js-generate").on( "click", function() {
+	  parseCommands(inputCommands, outputCommand);
+	});
+
+});
